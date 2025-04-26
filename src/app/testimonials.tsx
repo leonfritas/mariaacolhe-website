910f01de08ec4e7dcb0edb6fc2698ab9e4e386cb
Leo <leon_fritas@hotmail.com>
2025-04-26 13:34:16 -0400

    Remover aspas desnecessárias no texto dos depoimentos

--- src/app/testimonials.tsx
+++ src/app/testimonials.tsx
@@ -32,7 +32,7 @@ export const Testimonials: React.FC<TestimonialsProps> = (props) => {
                         />
                       </div>
                       <div className="testimonial-content overflow-hidden">
-                        <p className="mb-0 text-sm italic">"{d.text}"</p>
+                        <p className="mb-0 text-sm italic">{d.text}</p>
                         <div className="testimonial-meta mt-2 text-sm font-semibold text-[#666]">
                           - {d.name}
                         </div>
