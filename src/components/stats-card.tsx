fa7a3bcd9a43e19997d24b5ab21527f62232de67
Leo <leon_fritas@hotmail.com>
2025-04-27 00:51:42 -0400

    ajustar formatação e propriedades do componente StatsCard para compatibilidade com a versão 3

--- src/components/stats-card.tsx
+++ src/components/stats-card.tsx
@@ -1,5 +1,5 @@
-
 import { Card } from "@material-tailwind/react";
+
 interface StatsCardProps {
   count: string;
   title: string;
@@ -7,16 +7,21 @@ interface StatsCardProps {
 
 export function StatsCard({ count, title }: StatsCardProps) {
   return (
-    <Card color="transparent" shadow={false}>
+    <Card 
+      color="transparent" 
+      shadow={false}
+      placeholder={undefined} // Prop obrigatória no v3+
+      onPointerEnterCapture={undefined} // Prop obrigatória no v3+
+      onPointerLeaveCapture={undefined} // Prop obrigatória no v3+
+    >
       <h2 className="font-bold" color="blue-gray">
         {count}
       </h2>
-      <h2  color="blue-gray" className="mt-1 font-medium">
+      <h2 color="blue-gray" className="mt-1 font-medium">
         {title}
       </h2>
     </Card>
   );
 }
-
 
 export default StatsCard;
